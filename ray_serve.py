eaaf99758a6f06b08a6fc3833db95ad9346b2cf6
damontfgz <36029809+damontfgz@users.noreply.github.com>
2025-08-19 20:14:14 -0700

    testing

---
+++ ray_serve.py
@@ -0,0 +1,18 @@
+from ray import serve
+from ray.serve.llm import LLMConfig, build_openai_app
+
+llm_config = LLMConfig(
+    model_loading_config=dict(
+        model_id="qwen-0.5b",
+        model_source="Qwen/Qwen2.5-0.5B-Instruct",
+    ),
+    deployment_config=dict(
+        autoscaling_config=dict(
+            min_replicas=1, max_replicas=2,
+        )
+    ),
+    log_engine_metrics=True
+)
+
+app = build_openai_app({"llm_configs": [llm_config]})
+serve.run(app, blocking=True)
